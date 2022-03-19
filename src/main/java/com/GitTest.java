b78dcae9cb78c4e2078cde2a7bb4e3c947309dfd
adore <adore@hpu.com>
2022-03-19 21:32:01 +0800

    master  new commit

--- src/main/java/com/GitTest.java
+++ src/main/java/com/GitTest.java
@@ -1,6 +1,10 @@
 package com;
 
 public class GitTest {
+    /**
+     *
+     * @param args
+     */
     public static void main(String args[]){
         System.out.println("hello git");
         System.out.println(" hello git again!");

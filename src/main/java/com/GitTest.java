4001a375e9f07cbeed06816ef5fbff8e3b6770cb
韩俊豪 <72867027+I-adore-you@users.noreply.github.com>
2022-03-19 22:06:10 +0800

    Update GitTest.java

--- src/main/java/com/GitTest.java
+++ src/main/java/com/GitTest.java
@@ -15,6 +15,17 @@ public class GitTest {
         System.out.println("22222222222222222222");
 //         这是 一些 注释信息
         System.out.println("family");
-    }
+/**
+注意 在 pull的时候 要注意 一些 问题 在里面  
+1.注意  合并 冲突 这种 情况  conflict 
+这种原因  其实 和 在合并分支 情况 是一样的  master 分支 和 hot-fix两个分支 的代码 都发生了改变 这个  时候 合并 就会产生 冲突 conflict
+pull是 拉去 远程仓库  代码 到本地 如果 远程仓库 代码 跟本地代码不一致 会自动合并 所以 差生下面情况
+ pull 这种情况 也是这样  就是 你在  远程仓库 和 本地 仓库 都修改 了代码 这种情况 就会差生 合并冲突 需要手动合并
+
+
+*/
+        System.out.println("let me down");
+
+}
 
 }

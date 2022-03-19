58e4bbabe9cf2b26d5d209f758f03b580a636d10
adore <adore@hpu.com>
2022-03-19 21:02:02 +0800

    hot-fix commit

--- src/main/java/com/GitTest.java
+++ src/main/java/com/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println(" hello git again!");
         System.out.println(" hello servlet!");
+        System.out.println("加油 ~ just do yourself");
     }
 
 }
